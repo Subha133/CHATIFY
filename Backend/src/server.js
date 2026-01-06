5d7727412911fa0a5b9fd48d93c765c8492ba873
Subhajit Maity <maitysubhajit872@gmail.com>
2026-01-06 08:14:52 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 const app = express();
+const _dirname = path.resolve();
 
 console.log(process.env.PORT);
 
 app.use("/api/auth",authRoutes);
 app.use("/api/auth",messageRoutes);
+
+//make ready for deployment
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "../chatify/dist")))
+
+    app.get("*",(_, res)=>{
+        res.sendFile(path.join(_dirname, "../chatify","dist","index.html"));
+    })
+}
 
 
 const PORT = process.env.PORT || 3000;
